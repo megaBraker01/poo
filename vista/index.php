8b7f128d3ac644486a2c1769ec031361b5066d20
angel <angel_rafael01@hotmail.com>
2017-12-05 19:27:03 +0100

    nuestro primer commit

---
+++ vista/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Index</title>
+<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+<div class="cuerpo">
+	<div class="cabecera">
+    </div>
+    <div class="botonera">
+    </div>
+    <div class="contenido">
+    </div>
+</div>
+</body>
+</html>
